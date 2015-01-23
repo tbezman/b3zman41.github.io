087390b5ba84504e726afb4153b051c60717f916
b3zman41 <terencebezman@gmail.com>
2015-01-22 19:13:17 -0500

    Google Analytics and NPM update

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@
  *
  */
 
- 'use strict';
+'use strict';
 
 // Include Gulp & Tools We'll Use
 var gulp = require('gulp');
@@ -29,35 +29,35 @@ var pagespeed = require('psi');
 var reload = browserSync.reload;
 
 var AUTOPREFIXER_BROWSERS = [
-'ie >= 10',
-'ie_mob >= 10',
-'ff >= 30',
-'chrome >= 34',
-'safari >= 7',
-'opera >= 23',
-'ios >= 7',
-'android >= 4.4',
-'bb >= 10'
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
 ];
 
 // Lint JavaScript
 gulp.task('jshint', function () {
   return gulp.src('app/scripts/**/*.js')
-  .pipe(reload({stream: true, once: true}))
-  .pipe($.jshint())
-  .pipe($.jshint.reporter('jshint-stylish'))
-  .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Optimize Images
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-  .pipe($.cache($.imagemin({
-    progressive: true,
-    interlaced: true
-  })))
-  .pipe(gulp.dest('dist/images'))
-  .pipe($.size({title: 'images'}));
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}));
 });
 
 // Copy All Files At The Root Level (app)
@@ -66,48 +66,48 @@ gulp.task('copy', function () {
     'app/*',
     '!app/*.html',
     'node_modules/apache-server-configs/dist/.htaccess'
-    ], {
-      dot: true
-    }).pipe(gulp.dest('dist'))
-  .pipe($.size({title: 'copy'}));
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'copy'}));
 });
 
 // Copy Web Fonts To Dist
 gulp.task('fonts', function () {
   return gulp.src(['app/fonts/**'])
-  .pipe(gulp.dest('dist/fonts'))
-  .pipe($.size({title: 'fonts'}));
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe($.size({title: 'fonts'}));
 });
 
 // Compile and Automatically Prefix Stylesheets
 gulp.task('styles', function () {
   // For best performance, don't add Sass partials to `gulp.src`
   return gulp.src([
-    'app/styles/*.scss',
-    'app/styles/**/*.css',
-    'app/styles/components/components.scss'
+      'app/styles/*.scss',
+      'app/styles/**/*.css',
+      'app/styles/components/components.scss'
     ])
-  .pipe($.changed('styles', {extension: '.scss'}))
-  .pipe($.rubySass({
-    style: 'expanded',
-    precision: 10
-  })
-  .on('error', console.error.bind(console))
-  )
-  .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-  .pipe(gulp.dest('.tmp/styles'))
+    .pipe($.changed('styles', {extension: '.scss'}))
+    .pipe($.rubySass({
+        style: 'expanded',
+        precision: 10
+      })
+      .on('error', console.error.bind(console))
+    )
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp/styles'))
     // Concatenate And Minify Styles
     .pipe($.if('*.css', $.csso()))
     .pipe(gulp.dest('dist/styles'))
     .pipe($.size({title: 'styles'}));
-  });
+});
 
 // Scan Your HTML For Assets & Optimize Them
 gulp.task('html', function () {
   var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
   return gulp.src('app/**/*.html')
-  .pipe(assets)
+    .pipe(assets)
     // Concatenate And Minify JavaScript
     .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
     // Remove Any Unused CSS
@@ -115,13 +115,13 @@ gulp.task('html', function () {
     // the next line to only include styles your project uses.
     .pipe($.if('*.css', $.uncss({
       html: [
-      'app/index.html',
-      'app/styleguide.html'
+        'app/index.html',
+        'app/styleguide.html'
       ],
       // CSS Selectors for UnCSS to ignore
       ignore: [
-      /.navdrawer-container.open/,
-      /.app-bar.open/
+        /.navdrawer-container.open/,
+        /.app-bar.open/
       ]
     })))
     // Concatenate And Minify Styles
@@ -136,7 +136,7 @@ gulp.task('html', function () {
     // Output Files
     .pipe(gulp.dest('dist'))
     .pipe($.size({title: 'html'}));
-  });
+});
 
 // Clean Output Directory
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
@@ -153,9 +153,9 @@ gulp.task('serve', ['styles'], function () {
   });
 
   gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/app/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['app/app/images/**/*'], reload);
+  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], reload);
 });
 
 // Build and serve the output from the dist build
